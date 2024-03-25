28ac141822bf7fc6dcefdd98f5bc5f460f01b4a4
SafAnwar <140514859+SafAnwar@users.noreply.github.com>
2024-03-25 12:10:33 +0000

    Wassup Mfs I did Exercise 1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,8 @@
 # Exercise 1
 # Your solution comes here
+
+number = str(input("Enter a five-digit integer: "))
+first_part = int(number[0]) + int(number[2]) + int(number[4])
+first_part = first_part * 10
+second_part = int(number[1]) + int(number[3])
+print(first_part + second_part)
